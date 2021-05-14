007ff17073505c84514b08ed0958453d1c243bf8
lawrencechan1410 <83324198+lawrencechan1410@users.noreply.github.com>
2021-05-14 11:16:49 +0800

    Add compose data

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'Cvtm32PTdYme1e1fN5StmZUCAm4UR-CX',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
